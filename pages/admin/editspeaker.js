a534f7defed5a0ae084ccee32b944836c383a821
Puneet <puneet@tech.abhaytechnos.com>
2021-03-13 13:58:25 +0530

    build1

--- pages/admin/editspeaker.js
+++ pages/admin/editspeaker.js
@@ -1,16 +1,27 @@
 import { useRouter } from 'next/router'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios';
+import Notiflix from "notiflix";
 export default function Addspeaker({data}) {
     const router = useRouter();
     const [speaker_name, setspeaker_name] = useState(data[0].speaker_name||"");
     const [speaker_desig, setspeaker_desig] = useState(data[0].speaker_desig||"");
     const [speaker_company, setspeaker_company] = useState(data[0].speaker_company||"");
     const [speaker_photo, setspeaker_photo] = useState(data[0].speaker_photo||"");
+    // console.log(speaker_photo);
 
+    useEffect(async () => {
+      console.log(speaker_photo)
+      let blob = await fetch(speaker_photo).then(r => r.blob());
+      var reader = new FileReader();
+      reader.readAsDataURL(blob)
+      reader.onload = (event) => {
+        setspeaker_photo(event.target.result);
+      };
+    });
     const speakeradd = async event => {
         event.preventDefault()
-        console.log(speaker_photo);
+        
         var formData = new FormData();
         formData.append('speaker_name', speaker_name);
         formData.append('speaker_desig', speaker_desig);
@@ -20,10 +31,14 @@ export default function Addspeaker({data}) {
         for (var key of formData.entries()) {
           console.log(key[0] );
       }
-      const ress=await axios.put("http://15.206.99.13:5000/speakers/"+router.query.id, formData);
+      let config = {
+        onUploadProgress: Notiflix.Loading.Circle()
+      }
+      const ress=await axios.put("http://127.0.0.1:5000/speakers/"+router.query.id, formData,config );
       const result = await ress;
-      console.log(result)
+      // console.log(result)
         if(result.data.affectedRows){
+          Notiflix.Loading.Remove();
             router.push('/admin/allspeakers')
         }
         else{
@@ -49,7 +64,17 @@ export default function Addspeaker({data}) {
   </div>
   <div className="form-group">
     <label >Photo</label>
-    <input type="file" className="form-control" name="speaker_photo" onChange={e => setspeaker_photo(e.target.files[0])}  required/>
+    
+    <input type="file" className="form-control" name="speaker_photo" onChange={(e) =>{
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0])
+      // console.log(reader);
+      reader.onload = (event) => {
+        setspeaker_photo(event.target.result);
+      };
+      
+    }}  required/>
+    <img className="form-control" src={speaker_photo} className="image"/>
   </div>
 
 
@@ -57,6 +82,14 @@ export default function Addspeaker({data}) {
 </form>
 
 </div>
+<style jsx>{`
+        .image {
+          height: 100px;
+          width:100px;
+          margin:10px
+        }
+
+      `}</style>
 </main>
 
 );
@@ -73,7 +106,7 @@ export async function getServerSideProps({ query }) {
         notFound: true,
       }
     }
-    console.log(data);
+    // console.log(data);
   
     return {
       props: { data }, // will be passed to the page component as props
