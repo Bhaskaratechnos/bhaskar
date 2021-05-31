876e6078f47a8ea659d35f99e1e328e85f6f3469
puneet-atechnos <puneet@tech.abhaytechnos.com>
2021-05-31 07:54:02 +0530

    social1

--- pages/user/webinar.js
+++ pages/user/webinar.js
@@ -1,5 +1,4 @@
 import cookie from 'js-cookie'
-import Head from 'next/head';
 import { useRouter } from 'next/router'
 import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
